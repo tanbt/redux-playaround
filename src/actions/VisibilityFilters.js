e4fd8e14a6b4aa809501da8ea0d19799faf29cda
Tan Bui <trungtanbui@gmail.com>
2020-04-12 21:49:56 +0300

    dispatch set visibility event

--- src/actions/VisibilityFilters.js
+++ src/actions/VisibilityFilters.js
@@ -2,4 +2,9 @@ export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_COMPLETED: 'SHOW_COMPLETED',
   SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
+};
+
+export const setVisibilityFilter = filter => ({
+  type: 'SET_VISIBILITY_FILTER',
+  filter
+});
--- src/components/menu-bar.js
+++ src/components/menu-bar.js
@@ -1,4 +1,6 @@
 import { LitElement, html } from 'lit-element';
+import { store } from '../store.js';
+import { VisibilityFilters, setVisibilityFilter } from '../actions/VisibilityFilters.js';
 
 class MenuBar extends LitElement {
 
@@ -6,8 +8,23 @@ class MenuBar extends LitElement {
     return html`
       <div>
         menu-bar goes here.
+        <button @click="${() => this._setVisibility(VisibilityFilters.SHOW_ALL)}">
+          All
+        </button>
+
+        <button @click="${() => this._setVisibility(VisibilityFilters.SHOW_ACTIVE)}">
+          Active
+        </button>
+        <button @click="${() => this._setVisibility(VisibilityFilters.SHOW_COMPLETED)}">
+        Completed
+        </button>
       </div>
     `;
+  }
+
+  _setVisibility(filter) {
+    store.dispatch(setVisibilityFilter(filter));
+    console.log(store.getState());
   }
 
 }
